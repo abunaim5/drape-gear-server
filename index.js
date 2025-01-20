92dbfcf4aaef92595a98c1f85b7e1b3f1c3a8c22
Abu Naim <abunaim140@gmail.com>
2025-01-20 22:20:48 +0600

    create node express server with basic setup

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+
+// basic server apis
+app.get('/', (req, res) => {
+    res.send('DrapeGear Server is running');
+});
+
+app.listen(port, () => {
+    console.log(`DrapeGear server is running on PORT ${port}`);
+});
